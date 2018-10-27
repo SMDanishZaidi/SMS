71360f01ff808b8b31875eec4d7fed3af2e6b892
SMDanishZaidi <44525527+SMDanishZaidi@users.noreply.github.com>
2018-10-27 18:03:10 +0500

    danish 27-oct-2018
    fresh upload

---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SMS_1._0.Startup))]
+namespace SMS_1._0
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
